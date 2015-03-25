7c4e5f4cddfe4bfec7593c360698c9b9ca13e7c9
fengb <contact@fengb.info>
2015-03-24 22:33:43 -0500

    Refactored to demonstrate API more clearly

--- spec/spidr_test_spec.rb
+++ spec/spidr_test_spec.rb
@@ -2,54 +2,63 @@ require 'spec_helper'
 require 'spidr_test'
 
 RSpec.describe SpidrTest do
-  subject do
-    SpidrTest.new do |test|
+  it 'invokes test app' do
+    expect(TestRackApp).to receive(:call)
+
+    SpidrTest.crawl do |test|
       test.app = TestRackApp
     end
   end
 
-  it 'invokes test app' do
-    expect(TestRackApp).to receive(:call)
-    subject.crawl!
-  end
-
   it 'hits all routes' do
     crawled = []
-    subject.spidr.every_page do |page|
-      crawled << page.url.path
+
+    SpidrTest.crawl do |test|
+      test.app = TestRackApp
+      test.spidr.every_page do |page|
+        crawled << page.url.path
+      end
     end
-    subject.crawl!
 
     expect(crawled).to contain_exactly('/', '/foo', '/bar')
   end
 
   it 'generates the bodies' do
     pages = {}
-    subject.spidr.every_page do |page|
-      pages[page.url.path] = page.body
+
+    SpidrTest.crawl do |test|
+      test.app = TestRackApp
+      test.spidr.every_page do |page|
+        pages[page.url.path] = page.body
+      end
     end
-    subject.crawl!
 
     expect(pages).to include('/foo' => 'Foo', '/bar' => 'Bar')
   end
 
   it 'generates the bodies' do
     bodies = {}
-    subject.spidr.every_page do |page|
-      bodies[page.url.path] = page.body
+
+    SpidrTest.crawl do |test|
+      test.app = TestRackApp
+      test.spidr.every_page do |page|
+        bodies[page.url.path] = page.body
+      end
     end
-    subject.crawl!
 
     expect(bodies).to include('/foo' => 'Foo', '/bar' => 'Bar')
   end
 
   it 'detects status codes' do
     codes = {}
-    subject.path = '/status/common'
-    subject.spidr.every_page do |page|
-      codes[page.url.path] = page.code
+
+    SpidrTest.crawl do |test|
+      test.app = TestRackApp
+      test.path = '/status/common'
+      test.spidr.every_page do |page|
+        codes[page.url.path] = page.code
+      end
     end
-    subject.crawl!
 
     expect(codes).to include('/status/200' => 200, '/status/404' => 404, '/status/500' => 500)
   end
