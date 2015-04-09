49b905d2e86703b80edf9e18d0a54960c8f53e2e
fengb <contact@fengb.info>
2015-04-08 20:39:46 -0500

    Convert cucumber helpers to build real object

--- lib/spidr_test/cucumber_helpers.rb
+++ lib/spidr_test/cucumber_helpers.rb
@@ -2,17 +2,17 @@ require 'spidr_test'
 
 module SpidrTest
   module CucumberHelpers
-    def spidr_test_options(additional_options = {})
-      @spidr_test_options ||= {}
-      @spidr_test_options.merge!(additional_options)
+    def spidr_test
+      @spidr_test ||= SpidrTest::Runner.new(context: self)
     end
   end
 end
 
 Given 'SpidrTest is crawling "$app" starting at "$path"' do |app_name, path|
-  spidr_test_options(app: eval(app_name), path: path)
+  spidr_test.app = eval(app_name)
+  spidr_test.path = path
 end
 
 Then 'SpidrTest crawls' do
-  SpidrTest.crawl(spidr_test_options(context: self))
+  spidr_test.crawl!
 end
